b64fb56a2c4275f94bdaab822f22867a91368b9b
Marumo Thobejane <lennythobejane@gmail.com>
2021-04-04 00:54:29 +0200

    interface done

---
+++ map.js
@@ -0,0 +1,6 @@
+var map = document.getElementById("map");
+
+map.style.with = document.width;
+map.style.height = "400px";
+
+// making the map to display in the map section and also display my location
